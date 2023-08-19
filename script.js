d3073ed89ad7ed9a27a2a8be5dd8b486c2e8dc4f
Daniela Rocha <62150491+DanielaRocha25@users.noreply.github.com>
2023-08-19 20:26:02 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function inserir(num) {
-    let numero = document.getElementById('resultado').innerHTML;
-    document.getElementById('resultado').innerHTML = numero + num;
+    let digito = document.getElementById('resultado').innerHTML;
+    document.getElementById('resultado').innerHTML = digito + num;
 }
 function calc() {
-    let resultado = document.getElementById('resultado').innerHTML;
+    var resultado = document.getElementById('resultado').innerHTML;
     document.getElementById('resultado').innerHTML = eval(resultado);
 }
 function limpa() {
-    document.getElementById('resultado').innerHTML = "";
+    document.getElementById('resultado').innerHTML = null;
 }
